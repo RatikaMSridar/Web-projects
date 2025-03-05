f6c9ec3045cf458a16c65061e0acbe3560095475
Ratika M Sridar <ratikamsridar@gmail.com>
2025-03-05 10:58:01 +0530

    Saved local changes before pull

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Web-projects/",  // Replace with your actual repo name
   plugins: [react()],
-})
+});
